bde18c006a1c474866f6f86995e15abeb046c996
ineskd31 <ines.kaddi@hotmail.fr>
2023-03-08 13:50:18 +0100

    tart

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1>HOME</h1>
+            <p>test 1</p>
+            <p>test 2</p>
+            <p>test 3</p>
+        </div>
+    )
+}
